0ad90e8615eb98e694f8e2e5140bd7603a81c8bf
vic-lee <96267768+vic-lee@users.noreply.github.com>
2022-10-09 13:22:57 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
+// 1. Adidas Yeezy
+//Add an event listener to the Adidas Brand element (id of brand1) that will toggle the description of the shoe when clicked.
+
+
+
+// 2. Adidas x Allbirds
+//Add an event listener to the Adidas x AllbirdsBrand element (id of brand2) that will toggle the description of the shoe when clicked.
+
+
+
+//3. Adidas Human Race
+//Add an event listener to the Quantity element for the Adidas Tokio Solar Hu shoe that will increase the Quantity by 1 each time it is clicked.
+
+
+
+//4. Nike Air Jordan
+//Add an event listener to the Price element for the Nike Air Jordan shoe that will change the price from USD to JPY when clicked and vice versa.
+//(you can use '¥43,618' for the Yen amount )
+
+
+//Nike Air Mags
+//Add an event listener to the img element for the Nike Air Mag Shoe that will change the shoe image to an iconic movie that made this shoe popular after hovering (mouseover) over the shoe image and vice versa.
+
+//Use this image for the iconic movie"
+//'https://static.wikia.nocookie.net/bttf/images/f/fd/Back_To_The_Future_Nike_Shoes.jpg/'
+
+
+
+//6. Nike Kyrie
+//Add an event listener to all of the Nike shoes (Air Jordan, Air Mags, and Kyrie) that will toggle the description of the respective shoe when the brand element is clicked.
+
+
+
+
+
+
+
 

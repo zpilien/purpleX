00a77852231eb8ac032fc2fc3666f02757e15f14
vic-lee <96267768+vic-lee@users.noreply.github.com>
2022-10-10 16:53:26 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,37 +2,48 @@
 //Add an event listener to the Adidas Brand element (id of brand1) that will toggle the description of the shoe when clicked.
 
 
-
 // 2. Adidas x Allbirds
 //Add an event listener to the Adidas x AllbirdsBrand element (id of brand2) that will toggle the description of the shoe when clicked.
 
 
-
 //3. Adidas Human Race
-//Add an event listener to the Quantity element for the Adidas Tokio Solar Hu shoe that will increase the Quantity by 1 each time it is clicked.
-
+//Add an event listener to the Quantity element for the Adidas Tokio Solar Hu shoe that will increase the Quantity by 1 each time Quantity is clicked.
 
 
 //4. Nike Air Jordan
-//Add an event listener to the Price element for the Nike Air Jordan shoe that will change the price from USD to JPY when clicked and vice versa.
-//(you can use '¥43,618' for the Yen amount )
+//Add an event listener to the Price element for the Nike Air Jordan shoe that will change the price from USD to JPY when clicked and vice versa. (you can use '¥43,618' for the Yen amount )
 
 
-//Nike Air Mags
+//5. Nike Air Mags
 //Add an event listener to the img element for the Nike Air Mag Shoe that will change the shoe image to an iconic movie that made this shoe popular after hovering (mouseover) over the shoe image and vice versa.
 
-//Use this image for the iconic movie"
+//Use this image for the iconic movie: 
 //'https://static.wikia.nocookie.net/bttf/images/f/fd/Back_To_The_Future_Nike_Shoes.jpg/'
-
 
 
 //6. Nike Kyrie
 //Add an event listener to all of the Nike shoes (Air Jordan, Air Mags, and Kyrie) that will toggle the description of the respective shoe when the brand element is clicked.
 
 
+//7. Saucony Shadow
+//Add an event listener to the Quantity elements for the Saucony and Asics and shoes that will increase the Quantity by 1 each time Quantity is clicked.
 
 
+//8. Asics Onitsuka Tiger
+//Add an event listener to the Price elements of Saucony, Asics and Pizza Hut shoes that will toggle the price of the respective shoe when clicked.
 
 
+//9. Pizza Hut
+//There are a couple of missing HTML elements that you'll need to create. Please create a div element with the following details:
+//the div element will have an id of 'qtyItems'
+//the innerHTML will be 'Quantity'
+//add an event listener with a click action and function name of editTotalOrders
+//append this div element to Pizza Hut shoe 
 
+//Next, create a paragraph element with following details:
+//the p element will have an id of 'tally'
+//the innerHTML will be '1'
+//append this p element to the above div element that you just created
+
+//Last step, create a function editTotalOrders that will that will increase the Quantity by 1 each time Quantity is clicked.
 
